b39ad8db4bcb4ecbddbde0b4eba1d0a7088c1456
Swapnil Maheshwari <swapnilmaheshwari5@outlook.com>
2022-11-10 08:13:14 +0530

    Conflict changes test

--- src/base/PredefinedActions.java
+++ src/base/PredefinedActions.java
@@ -1,5 +1,32 @@
 package base;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
 public class PredefinedActions {
+	
+	protected static  WebDriver driver;
+	
+	public static void start(String url) {
+		System.setProperty("webdriver.chrome.driver","drivers/chromedriver_106.exe>>>>>>>>>>");
+		System.out.println("...................................");
+		System.out.println("Step - Launch chrome browser");
+		driver=new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	}
+	
+	public static void closeBrowser() {
+		driver.close();
+	}
+	public static void closeAllBrowser() {
+		driver.quit();
+	}
 
 }
--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,50 @@
 package pages;
 
-public class LoginPage {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import base.PredefinedActions;
+
+public class LoginPage extends PredefinedActions  {
+	
+	public void login(String username , String password) {
+		enterUsername(username);
+		enterPassword(password);
+		clickOnLoginBtn();
+	}
+	
+	public void enterUsername(String username) {
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
+		
+	}
+	
+	public void enterPassword(String password) {
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
+		
+	}
+	
+	public void clickOnLoginBtn() {
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+	
+	public boolean isUsernameErrorMessage() {
+		WebElement usernameErrorMessage=driver.findElement(By.cssSelector("#txtUsername-error"));
+		return usernameErrorMessage.isDisplayed();
+	}
+	
+	public boolean isPasswordErrorMessage() {
+		WebElement passwordErrorMessage =driver.findElement(By.cssSelector("#txtPassword-error"));
+		return passwordErrorMessage.isDisplayed();
+	}
+	
+	public boolean isLogoDisplayed() {
+		return driver.findElement(By.cssSelector("div.organization-logo.shadow>img")).isDisplayed();
+		
+	}
+	
+	public String getPageTitle() {
+		return driver.getTitle();
+	}
 }

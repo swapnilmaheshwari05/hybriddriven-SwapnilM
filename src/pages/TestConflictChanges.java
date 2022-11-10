2170c175dd8c31cc97d0516cb8f6d8edb640374b
Swapnil Maheshwari <swapnilmaheshwari5@outlook.com>
2022-11-10 09:03:01 +0530

    New file created

---
+++ src/pages/TestConflictChanges.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class TestConflictChanges {
+	
+	public void m1() {
+		System.out.println("Testing for m1 method");
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("Bye");
+		System.out.println("File created for checking conflict changes");
+	}
+
+}
